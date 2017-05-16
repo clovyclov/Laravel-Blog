23fe26c911e4bc30c9a9ecb63f79b243d35c0a50
Clovis <shropshireclovis@gmail.com>
2017-05-15 22:11:22 -0400

    Updated PagesController and about view to include full name variables

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    About Us
+                    About {{$fullname}}
                 </div>
 
                 <div class="links">
